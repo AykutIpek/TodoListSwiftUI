e917f43d4bb377bea3490d7573c8cca093592a6c
AykutIpek <aykutipek_2001@hotmail.com>
2023-06-27 21:49:30 +0300

    Initial Commit

---
+++ TodoListSwiftUI/TodoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftUIApp.swift
+//  TodoListSwiftUI
+//
+//  Created by aykut ipek on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
